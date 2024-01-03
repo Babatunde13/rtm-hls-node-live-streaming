7f595b5f80a51a96a765f920b1a141150ead87a5
Babatunde Koiki <koikibabatunde14@gmail.com>
2024-01-03 09:34:43 +0100

    set up live stream with rtmp and nginx server, obs for streaming and express for auth

---
+++ auth/server.ts
@@ -0,0 +1,30 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+const STREAM_KEY = process.env.STREAM_KEY || "supersecret";
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (_, res) => {
+  res.send("Hello World!");
+});
+
+app.post("/auth", (req, res) => {
+    const streamKey = req.body.key;
+    console.log(streamKey);
+
+    if (streamKey === STREAM_KEY) {
+        res.send("OK");
+    } else {
+        res.status(401).send("Unauthorized");
+    }
+});
+
+app.listen(8000, () => {
+  console.log("Server running on port 8000");
+});
